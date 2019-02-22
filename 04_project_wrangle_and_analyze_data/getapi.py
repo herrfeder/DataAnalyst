4a82047d54c6ed7ae0a39cf6c8331411d89b91e1
David Lassig <d.lassig@t-online.de>
2019-02-22 09:59:59 +0100

    Remove API keys from getapi.py

--- 04_project_wrangle_and_analyze_data/getapi.py
+++ 04_project_wrangle_and_analyze_data/getapi.py
@@ -1,10 +1,10 @@
 import tweepy
 
 
-consumer_key = '8y3JayTZMuulHAjN2G0qFaVA0'
-consumer_secret = 'D6MvjTIVPYFqgf6jHzKfpQJoxleIsusDvGCirKHFzx8B6Zkk52'
-access_token = '859623696780201984-TAPoYVCv1fevDLD8H6rmM5REdcrbfYr'
-access_secret = 'sTnTGvtR0NDnCfADh7eDp3Yrh4dkqLgtSOCRqMHz157q7'
+consumer_key = 'XXX'
+consumer_secret = 'XXX'
+access_token = 'XXX'
+access_secret = 'XXX'
 
 
 def get_twitter_api():
